cc07f0a8bc2ba19e6bfab838fe3d51825df84ee9
Jose Sanchez <josesanchez4512@gmail.com>
2018-10-25 19:25:02 -0500

    gitolympic

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,17 @@
+ 'use strict'
+const listOfOlympians = ['White', 'Phelps', 'Jose', 'Michael',"Peter"]
+ 
+
+
+array1.forEach(function(element) {
+    console.log(element);
+  });
+ 
+  const printListOfOlympians = (arr) => {
+      arr.forEach(LogIt(arr) {
+
+      }
+  printListOfOlympians('Luis', 'Carlos')
+
+
+vbd
